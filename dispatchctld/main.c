d9ab7a65b352edf3f8f8ec05a5f49bc9c04dbd9b
Nick Spain <nicholas.spain96@gmail.com>
2019-12-11 20:58:23 +1100

    Ignore unused vars

--- dispatchctld/main.c
+++ dispatchctld/main.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
 #include <czmq.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
     int major, minor, patch;
     zmq_version(&major, &minor, &patch);
     printf("Using version %d.%d.%d of zmq\n", major, minor, patch);
--- dispatchd/main.c
+++ dispatchd/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello, World!\n");
+    (void)argc;
+    (void)argv;
+    printf("Hello, World!\n");
 }
