b04fb205c71e945a132115ddec6fcf961e1dd91a
Nick Spain <nicholas.spain96@gmail.com>
2019-12-14 01:06:44 +1100

    Use cmake instead and add conan

--- dispatchctld/main.c
+++ dispatchctld/main.c
@@ -1,15 +1,21 @@
 #include <assert.h>
-#include <bool.h>
-#include <czmq.h>
 #include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include <zmq.h>
+#include <msgpack.h>
+
+const char *IPC_PATH_DEFAULT = "/tmp/dispatch/0";
+const char *LOG_FILE_DEFAULT = "stderr";
+const char *PID_FILE_DEFAULT = "/var/run/dispatch/dispatch.pid";
+
 struct Args {
     char *conf_file;
-    bool daemon;
-    bool help;
+    int daemon;
+    int help;
     char *ipc_path;
     char *log_file;
     char *pid_file;
@@ -18,52 +24,55 @@ struct Args {
 void info(void);
 
 int main(int argc, char *argv[]) {
-    static struct option long_opts[] = {
-        {"conf_file", optional_argument, 0, 'c'},
-        {"daemon", no_argument, 0, 'd'},
-        {"help", no_argument, 0, "h"},
-        {"ipc_path", optional_argument, "/tmp/dispatch/0", "i"},
-        {"log_file", optional_argument, "stderr", "l"},
-        {"pid_file", optional_argument, "/var/run/dispatch/dispatch.pid"},
-        {NULL, 0, 0, 0}};
     char *app_name = argv[0];
+    struct Args args = (struct Args){
+                              .conf_file = NULL,
+                              .daemon = 0,
+                              .help = 0,
+                              .ipc_path = "/tmp/dispatch/0",
+                              .log_file = "stderr",
+                              .pid_file = "/var/run/dispatch/dispatch.pid",
+    };
+    static struct option long_opts[] = {
+        {"conf-file", optional_argument, 0, 'c'},
+        {"daemon", no_argument, 0, 'd'},
+        {"help", no_argument, 0, 'h'},
+        {"ipc-path", optional_argument, 0, 'i'},
+        {"log-file", optional_argument, 0, 'l'},
+        {"pid-file", optional_argument, 0, 'p'},
+        {0, 0, 0, 0}
+    };
     int value, option_index = 0;
-    struct Args args;
-    while((value = getopt_long(argc, argv, "c:d:h:i:l:p", long_opts, &option_index)) != -1) {
-      switch(value) {
-      case 'c':
-        args.conf_file = strdup(optarg);
-        break;
-      case 'd':
-        args.daemon = true;
-        break;
-      case 'h':
-        info();
-        return EXIT_FAILURE;
-        break;
-      case 'i':
-        args.ipc_path = strdup(optarg);
-        break;
-      case 'l':
-        args.log_file = strdup(optarg);
-        break;
-      case 'p':
-        args.pid_file = strdup(optarg);
-        break;
-      default:
-        fprintf(stderr, "Argument %s is not known\n", optarg);
-        exit(1);
-      }
+    while ((value = getopt_long(argc, argv, "c::dhi::l::p::", long_opts,
+                                &option_index)) != -1) {
+        switch (value) {
+        case 'h':
+            info();
+            return EXIT_FAILURE;
+        case 'c':
+            args.conf_file = strdup(optarg);
+            break;
+        case 'd':
+            args.daemon = true;
+            break;
+        case 'i':
+            args.ipc_path = strdup(optarg);
+            break;
+        case 'l':
+            args.log_file = strdup(optarg);
+            break;
+        case 'p':
+            args.pid_file = strdup(optarg);
+            break;
+        }
     }
 
-    void *ctx = zmq_ctx_new();
-    assert(ctx);
-
-    void *replier = zmq_socket(ctx, ZMQ_REP);
-    assert(replier);
-
-    int rc = zmq_bind(replier, "ipc:///tmp/dispatch/0");
-    assert(c == 0);
+    puts("Settings");
+    printf("config file: %s\n", args.conf_file);
+    printf("daemon: %s", args.daemon ? "yes" : "no");
+    printf("ipc path: %s\n", args.ipc_path);
+    printf("log file: %s\n", args.log_file);
+    printf("pid file: %s\n", args.pid_file);
 }
 
 void info() {
@@ -72,4 +81,5 @@ void info() {
     int major, minor, patch;
     zmq_version(&major, &minor, &patch);
     printf("zmq: v%d.%d.%d\n", major, minor, patch);
+    printf("msgpack: v%s\n", MSGPACK_VERSION);
 }
